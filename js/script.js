23aa0cb7dc0f333dec70e18c238d78be5991d380
amsornet <amsornet@google.com>
2024-06-19 17:14:03 +0300

    init js click notice__close

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const noticeEl = document.querySelector('.notice');
+
+if (noticeEl) {
+    const noticeCloseEl = noticeEl.querySelector('.notice__close');
+    noticeCloseEl.addEventListener('click', () => {
+        noticeEl.classList.add('notice_hidden');
+
+    });
+
+}
